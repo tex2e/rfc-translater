594ced3eb280de61cf30e8624454666a7d595ef8
Mako <mnfeconicu41@gmail.com>
2019-10-16 17:14:06 +0900

    main.py: continuous process

--- main.py
+++ main.py
@@ -3,31 +3,37 @@ import sys
 from src.fetch_rfc import fetch_rfc
 from src.trans_rfc import trans_rfc
 from src.make_html import make_html
+from src.fetch_index import diff_remote_and_local_index
 
 def main(rfc_number):
     fetch_rfc(rfc_number)
     trans_rfc(rfc_number)
     make_html(rfc_number)
 
+def continuous_main(maximum=10):
+    numbers = diff_remote_and_local_index()
+    for rfc_number in list(numbers)[:10]:
+        main(rfc_number)
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('rfc_number', type=int)
-    parser.add_argument('--fetch', action='store_true')
-    parser.add_argument('--trans', action='store_true')
-    parser.add_argument('--make', action='store_true')
+    parser.add_argument('--rfc', type=int, help='RFC number')
+    parser.add_argument('--fetch', action='store_true', help='only fetch RFC')
+    parser.add_argument('--trans', action='store_true', help='only translate')
+    parser.add_argument('--make', action='store_true', help='only make HTML')
+    parser.add_argument('--continuous', action='store_true', help='continuous process')
+    parser.add_argument('--max', type=int, help='continuous process max time')
     args = parser.parse_args()
 
-    rfc_number = args.rfc_number
-    if args.fetch:
-        fetch_rfc(rfc_number)
-        sys.exit()
-    if args.trans:
-        trans_rfc(rfc_number)
-        sys.exit()
-    if args.make:
-        make_html(rfc_number)
-        sys.exit()
+    if args.fetch and args.rfc:
+        fetch_rfc(args.rfc)
+    elif args.trans and args.rfc:
+        trans_rfc(args.rfc)
+    elif args.make and args.rfc:
+        make_html(args.rfc)
 
-    main(rfc_number)
+    elif not args.continuous and args.rfc:
+        main(args.rfc)
+    else:
+        continuous_main(maximum=args.max)
