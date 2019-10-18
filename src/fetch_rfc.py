45cfc697ccea84b908aa9ab19449c43504c0893a
Mako <mnfeconicu41@gmail.com>
2019-10-18 13:51:55 +0900

    detect source code

--- src/fetch_rfc.py
+++ src/fetch_rfc.py
@@ -33,7 +33,8 @@ class Paragraph:
                 or text.find('___') >= 0
                 or text.find('.........') >= 0
                 or text.find('. . . . . . . ') >= 0
-                or text.find('/*') >= 0)
+                or text.find('/*') >= 0
+                or len(re.compile(r';$', re.MULTILINE).findall(text)) >= 2)
 
     def _find_section_title_pattern(self, text):
         # "N." が現れたときはセクションのタイトルとする
