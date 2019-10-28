072639d89e7f7b366e33da91ee268999002532e8
Mako <mnfeconicu41@gmail.com>
2019-10-28 17:10:25 +0900

    update index.js

--- html/index.js
+++ html/index.js
@@ -4,28 +4,40 @@ console.log('hello!');
 document.addEventListener('DOMContentLoaded', function () {
 
   var searchText = document.getElementById('searchText');
-  searchText.addEventListener('keyup', function (event) {
-    if (event.isComposing || event.keyCode === 229) return;
-    event.preventDefault();
 
-    // Search RFC by user input
-    var input, filter, listGroup, listItem, a, i, txtValue;
-    input = document.getElementById('searchText');
-    filter = input.value;
-    listGroup = document.getElementById("RFCs");
-    listItem = listGroup.getElementsByTagName('a');
+  if (searchText) {
 
-    console.log(filter);
+    searchText.addEventListener('keyup', function (event) {
+      event.preventDefault();
+      if (event.isComposing || event.keyCode === 229) return;
 
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < listItem.length; i++) {
-      txtValue = listItem[i].textContent || listItem[i].innerText;
-      if (txtValue.indexOf(filter) > -1) {
-        listItem[i].style.display = "";
-      } else {
-        listItem[i].style.display = "none";
+      // Search RFC by user input
+      var input, filter, listGroup, listItem, a, i, txtValue;
+      input = document.getElementById('searchText');
+      filter = input.value;
+      listGroup = document.getElementById("RFCs");
+      listItem = listGroup.getElementsByTagName('a');
+
+      console.log(filter);
+
+      // Loop through all list items, and hide those who don't match the search query
+      for (i = 0; i < listItem.length; i++) {
+        txtValue = listItem[i].textContent || listItem[i].innerText;
+        if (txtValue.indexOf(filter) > -1) {
+          listItem[i].style.display = "";
+        } else {
+          listItem[i].style.display = "none";
+        }
       }
-    }
-  });
+    });
+
+    searchText.addEventListener('keypress', function (event) {
+      var key = event.which || event.keyCode;
+      if (key === 13) { // Enterキーでページをリロードさせない
+        event.preventDefault();
+      }
+    });
+
+  }
 
 });
