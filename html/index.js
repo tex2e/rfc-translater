4b1acd9260950dd29ff32da8ba59d5adee15afeb
Mako <mnfeconicu41@gmail.com>
2019-10-28 11:16:46 +0900

    index.html: add search box

--- html/index.js
+++ html/index.js
@@ -1,2 +1,31 @@
 
 console.log('hello!');
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  var searchText = document.getElementById('searchText');
+  searchText.addEventListener('keyup', function (event) {
+    if (event.isComposing || event.keyCode === 229) return;
+    event.preventDefault();
+
+    // Search RFC by user input
+    var input, filter, listGroup, listItem, a, i, txtValue;
+    input = document.getElementById('searchText');
+    filter = input.value;
+    listGroup = document.getElementById("RFCs");
+    listItem = listGroup.getElementsByTagName('a');
+
+    console.log(filter + ', ' + listItem.length);
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < listItem.length; i++) {
+      txtValue = listItem[i].textContent || listItem[i].innerText;
+      if (txtValue.indexOf(filter) > -1) {
+        listItem[i].style.display = "";
+      } else {
+        listItem[i].style.display = "none";
+      }
+    }
+  });
+
+});
