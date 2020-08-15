5543d05f0d54bb14657c29f4d2ea57614a9bacd2
Mako <mnfeconicu41@gmail.com>
2020-08-15 19:55:54 +0900

    index.html: disable search form

--- html/index.js
+++ html/index.js
@@ -1,5 +1,5 @@
 
-console.log('hello!');
+// console.log('hello!');
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -7,42 +7,42 @@ document.addEventListener('DOMContentLoaded', function () {
   // index.html
   // ---------------------------------------------------------------------------
 
-  var searchText = document.getElementById('searchText');
+  // var searchText = document.getElementById('searchText');
 
-  if (searchText) {
+  // if (searchText) {
 
-    searchText.addEventListener('keyup', function (event) {
-      event.preventDefault();
-      if (event.isComposing || event.keyCode === 229) return;
+  //   searchText.addEventListener('keyup', function (event) {
+  //     event.preventDefault();
+  //     if (event.isComposing || event.keyCode === 229) return;
 
-      // Search RFC by user input
-      var input, filter, listGroup, listItem, a, i, txtValue;
-      input = document.getElementById('searchText');
-      filter = input.value;
-      listGroup = document.getElementById("RFCs");
-      listItem = listGroup.getElementsByTagName('a');
+  //     // Search RFC by user input
+  //     var input, filter, listGroup, listItem, a, i, txtValue;
+  //     input = document.getElementById('searchText');
+  //     filter = input.value;
+  //     listGroup = document.getElementById("RFCs");
+  //     listItem = listGroup.getElementsByTagName('a');
 
-      console.log(filter);
+  //     console.log(filter);
 
-      // Loop through all list items, and hide those who don't match the search query
-      for (i = 0; i < listItem.length; i++) {
-        txtValue = listItem[i].textContent || listItem[i].innerText;
-        if (txtValue.indexOf(filter) > -1) {
-          listItem[i].style.display = "";
-        } else {
-          listItem[i].style.display = "none";
-        }
-      }
-    });
+  //     // Loop through all list items, and hide those who don't match the search query
+  //     for (i = 0; i < listItem.length; i++) {
+  //       txtValue = listItem[i].textContent || listItem[i].innerText;
+  //       if (txtValue.indexOf(filter) > -1) {
+  //         listItem[i].style.display = "";
+  //       } else {
+  //         listItem[i].style.display = "none";
+  //       }
+  //     }
+  //   });
 
-    searchText.addEventListener('keypress', function (event) {
-      var key = event.which || event.keyCode;
-      if (key === 13) { // Enterキーでページをリロードさせない
-        event.preventDefault();
-      }
-    });
+  //   searchText.addEventListener('keypress', function (event) {
+  //     var key = event.which || event.keyCode;
+  //     if (key === 13) { // Enterキーでページをリロードさせない
+  //       event.preventDefault();
+  //     }
+  //   });
 
-  }
+  // }
 
   // ---------------------------------------------------------------------------
   // rfcXXXX.html
