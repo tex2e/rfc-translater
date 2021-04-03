bb507dcc0e08ca6b55fccdb4c61d025d786fcc83
Mako <mnfeconicu41@gmail.com>
2021-04-03 18:53:15 +0900

    add obsoleted_by

--- html/index.js
+++ html/index.js
@@ -4,53 +4,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   // ---------------------------------------------------------------------------
-  // index.html
-  // ---------------------------------------------------------------------------
-
-  // var searchText = document.getElementById('searchText');
-
-  // if (searchText) {
-
-  //   searchText.addEventListener('keyup', function (event) {
-  //     event.preventDefault();
-  //     if (event.isComposing || event.keyCode === 229) return;
-
-  //     // Search RFC by user input
-  //     var input, filter, listGroup, listItem, a, i, txtValue;
-  //     input = document.getElementById('searchText');
-  //     filter = input.value;
-  //     listGroup = document.getElementById("RFCs");
-  //     listItem = listGroup.getElementsByTagName('a');
-
-  //     console.log(filter);
-
-  //     // Loop through all list items, and hide those who don't match the search query
-  //     for (i = 0; i < listItem.length; i++) {
-  //       txtValue = listItem[i].textContent || listItem[i].innerText;
-  //       if (txtValue.indexOf(filter) > -1) {
-  //         listItem[i].style.display = "";
-  //       } else {
-  //         listItem[i].style.display = "none";
-  //       }
-  //     }
-  //   });
-
-  //   searchText.addEventListener('keypress', function (event) {
-  //     var key = event.which || event.keyCode;
-  //     if (key === 13) { // Enterキーでページをリロードさせない
-  //       event.preventDefault();
-  //     }
-  //   });
-
-  // }
-
-  // ---------------------------------------------------------------------------
   // rfcXXXX.html
   // ---------------------------------------------------------------------------
 
   var footer = document.getElementById('rfc_footer');
-
   if (footer) {
+    // 編集ページ表示方法
     var rfc_number = parseInt(document.getElementById('rfc_number').innerText);
     window.addEventListener('click', function (evt) {
       if (evt.detail === 4) {
@@ -60,6 +19,33 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       }
     });
+  }
+
+  var rfc_alert = document.getElementById('rfc_alert');
+  if (rfc_alert) {
+    // 対象RFCが廃止されたか確認し、廃止済みなら最新版RFCの場所を表示する。
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+      if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+        var rfc_number = parseInt(document.getElementById('rfc_number').innerText);
+        var data = JSON.parse(httpRequest.responseText);
+        var datum = data[rfc_number];
+        // console.log(datum);
+        if (datum && datum["obsoleted_by"]) {
+          rfc_alert.classList.remove("hidden");
+
+          var rfc_alert_obsoleted_by = document.getElementById('rfc_alert_obsoleted_by');
+          var rfc_links = "";
+          if (datum["obsoleted_by"].length >= 1) {
+            var rfc_number = datum["obsoleted_by"][0];
+            rfc_links += '<a href="./rfc' + rfc_number + '.html">RFC ' + rfc_number + '</a>';
+          }
+          rfc_alert_obsoleted_by.innerHTML = rfc_links;
+        }
+      }
+    };
+    httpRequest.open('GET', 'obsoletes.json');
+    httpRequest.send();
   }
 
 });
--- src/fetch_index.py
+++ src/fetch_index.py
@@ -2,8 +2,13 @@
 from lxml import html  # pip install lxml
 import requests
 
+import os
 import re
 import glob
+import json
+
+OUTPUT_DIR  = "html"
+OUTPUT_PATH = os.path.join(OUTPUT_DIR, "obsoletes.json")
 
 def fetch_remote_index():
     # 発行されているRFCの番号の一覧をページから取得する
@@ -27,11 +32,40 @@ def fetch_remote_index():
             continue
         rfcs.append(content)
 
-    rfc_numbers = []
+    rfc_numbers = [] # rfcの番号一覧
+    data = {} # 廃止・更新情報一覧
     for rfc in rfcs:
-        m = re.match(r'^RFC(\d+)', rfc)
+        #m = re.match(r'^RFC(\d+)', rfc)
+        m = re.match(r'^RFC(?P<rfc>\d+) (?P<content>.+)$', rfc)
         if m:
-            rfc_numbers.append(int(m[1]))
+            rfc_number = m["rfc"]
+            rfc_numbers.append(int(rfc_number))
+            content = m["content"]
+
+            obj = {}
+            m = re.search(r'\(Obsoletes (?P<obsoletes>[^)]+)\)', content)
+            if m:
+                obsoletes = list(map(get_rfc_number, m["obsoletes"].split(", ")))
+                obj["obsoletes"] = obsoletes
+            m = re.search(r'\(Obsoleted by (?P<obsoleted_by>[^)]+)\)', content)
+            if m:
+                obsoleted_by = list(map(get_rfc_number, m["obsoleted_by"].split(", ")))
+                obj["obsoleted_by"] = obsoleted_by
+            m = re.search(r'\(Updates (?P<updates>[^)]+)\)', content)
+            if m:
+                updates = list(map(get_rfc_number, m["updates"].split(", ")))
+                obj["updates"] = updates
+            m = re.search(r'\(Updated by (?P<updated_by>[^)]+)\)', content)
+            if m:
+                updated_by = list(map(get_rfc_number, m["updated_by"].split(", ")))
+                obj["updated_by"] = updated_by
+
+            # print(obj)
+            data[rfc_number] = obj
+
+    if os.path.isdir(OUTPUT_DIR):
+        with open(OUTPUT_PATH, 'w', encoding="utf-8", newline="\n") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
 
     return rfc_numbers
 
@@ -54,3 +88,10 @@ def diff_remote_and_local_index():
     local_index  = fetch_local_index()
     diff_index = set(remote_index) - set(local_index)
     return diff_index
+
+def get_rfc_number(string):
+    return string.strip().replace("RFC", "")
+
+
+if __name__ == '__main__':
+    print(fetch_remote_index())
