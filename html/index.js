1ab434c6d2e782403173944d02ef7620890a3b09
Mako <mnfeconicu41@gmail.com>
2021-04-09 18:31:17 +0900

    update index.js

--- html/index.js
+++ html/index.js
@@ -1,6 +1,4 @@
 
-// console.log('hello!');
-
 document.addEventListener('DOMContentLoaded', function () {
 
   // ---------------------------------------------------------------------------
@@ -23,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var rfc_alert = document.getElementById('rfc_alert');
   if (rfc_alert) {
-    // 対象RFCが廃止されたか確認し、廃止済みなら最新版RFCの場所を表示する。
+    // 対象RFCが廃止されたか確認し、廃止なら修正版RFCへのリンクを表示する。
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
       if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
@@ -37,8 +35,13 @@ document.addEventListener('DOMContentLoaded', function () {
           var rfc_alert_obsoleted_by = document.getElementById('rfc_alert_obsoleted_by');
           var rfc_links = "";
           if (datum["obsoleted_by"].length >= 1) {
-            var rfc_number = datum["obsoleted_by"][0];
-            rfc_links += '<a href="./rfc' + rfc_number + '.html">RFC ' + rfc_number + '</a>';
+            for (var i = 0; i < datum["obsoleted_by"].length; i++) {
+              var rfc_number = datum["obsoleted_by"][i];
+              rfc_links += '<a href="./rfc' + rfc_number + '.html">RFC ' + rfc_number + '</a>';
+              if (i + 1 < datum["obsoleted_by"].length) {
+                rfc_links += ", ";
+              }
+            }
           }
           rfc_alert_obsoleted_by.innerHTML = rfc_links;
         }
