431d6049912dce7771bf2506283f09f3e3ee9471
Mako <mnfeconicu41@gmail.com>
2024-02-18 08:05:04 +0900

    update main.py

--- main.py
+++ main.py
@@ -3,14 +3,11 @@
 # ------------------------------------------------------------------------------
 
 import sys
-import time
 from src.fetch_rfc import fetch_rfc, RFCNotFound
 from src.trans_rfc import trans_rfc
 from src.make_html import make_html
 from src.make_index import make_index, make_index_draft
 from src.fetch_index import diff_remote_and_local_index
-from src.make_json_from_html import make_json_from_html
-from src.fetch_wg import write_rfc_wg_list
 
 def main(rfc_number: int | str) -> None:
     print('[*] RFC %s:' % rfc_number)
@@ -29,8 +26,6 @@ def main(rfc_number: int | str) -> None:
 
 def continuous_main(begin=None, end=None, only_first=False):
     numbers = [x for x in diff_remote_and_local_index() if x >= 2220]
-    # print('[*] diff_remote_and_local:')
-    # print(numbers)
     if begin and end:  # 開始と終了区間の設定
         numbers = [x for x in numbers if begin <= x <= end]
     elif begin:  # 開始のみ設定
@@ -47,17 +42,17 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--rfc',         type=str,            help='RFC number            (ex. --rfc 8446)')
-    parser.add_argument('--fetch',       action='store_true', help='only fetch RFC        (ex. --rfc 8446 --fetch)')
-    parser.add_argument('--trans',       action='store_true', help='only translate        (ex. --rfc 8446 --trans)')
-    parser.add_argument('--make',        action='store_true', help='only make HTML        (ex. --rfc 8446 --fetch)')
-    parser.add_argument('--make-json',   action='store_true', help='make JSON from HTML   (ex. --make-json --rfc 8446)')
-    parser.add_argument('--begin',       type=int,            help='begin rfc number      (ex. --begin 8000)')
-    parser.add_argument('--end',         type=int,            help='end rfc number        (ex. --begin 8000 --end 9000)')
-    parser.add_argument('--make-index',  action='store_true', help='make html/index.html  (ex. --make-index)')
-    parser.add_argument('--force', '-f', action='store_true', help='ignore cache          (ex. --rfc 8446 --fetch --force)')
-    parser.add_argument('--only-first',  action='store_true', help='take only first RFC   (ex. --begin 8000 --only-first)')
-    parser.add_argument('--draft',       type=str,            help='take RFC draft        (ex. --draft draft-ietf-tls-esni-14)')
-    parser.add_argument('--fetch-group', action='store_true', help='fetch WG list         (ex. --fetch-group)')
+    parser.add_argument('--fetch',       action='store_true', help='Only fetch RFC        (ex. --rfc 8446 --fetch)')
+    parser.add_argument('--trans',       action='store_true', help='Only translate        (ex. --rfc 8446 --trans)')
+    parser.add_argument('--make',        action='store_true', help='Only make HTML        (ex. --rfc 8446 --fetch)')
+    parser.add_argument('--make-json',   action='store_true', help='Make JSON from HTML   (ex. --make-json --rfc 8446)')
+    parser.add_argument('--begin',       type=int,            help='Set begin rfc number  (ex. --begin 8000)')
+    parser.add_argument('--end',         type=int,            help='Set end rfc number    (ex. --begin 8000 --end 9000)')
+    parser.add_argument('--make-index',  action='store_true', help='Make html/index.html  (ex. --make-index)')
+    parser.add_argument('--force', '-f', action='store_true', help='Ignore cache          (ex. --rfc 8446 --fetch --force)')
+    parser.add_argument('--only-first',  action='store_true', help='Take only first RFC   (ex. --begin 8000 --only-first)')
+    parser.add_argument('--draft',       type=str,            help='Take RFC draft        (ex. --draft draft-ietf-tls-esni-14)')
+    parser.add_argument('--fetch-group', action='store_true', help='Write WG list         (ex. --fetch-group)')
     parser.add_argument('--make-index-draft',
                                          action='store_true', help='make draft/index.html (ex. --make-index-draft)')
     parser.add_argument('--transtest',   action='store_true')
@@ -69,6 +64,8 @@ if __name__ == '__main__':
     rfcs = None
     if args.rfc:
         rfcs = [int(rfc_number) for rfc_number in args.rfc.split(",")]
+    elif args.begin and args.end:
+        rfcs = list(range(args.begin, args.end))
     elif args.draft:
         rfcs = [args.draft]
 
@@ -83,23 +80,14 @@ if __name__ == '__main__':
     elif args.fetch_group:
         # WorkingGroupの一覧作成
         print("[*] WorkingGroupの一覧作成")
+        from src.fetch_wg import write_rfc_wg_list
         write_rfc_wg_list()
     elif args.transtest:
         # 翻訳のテスト
         from src.trans_rfc import trans_test
         res = trans_test()
         print('Translate test result:', res)
-    elif args.summarize and args.begin and args.end:
-        # 範囲指定でRFCの要約作成
-        from src.summarize_rfc import summarize_rfc
-        for rfc in range(args.begin, args.end):
-            print("[*] RFC %s を要約" % rfc)
-            if summarize_rfc(rfc, args.chatgpt, args.force):
-                # RFCのHTMLを作成
-                print("[*] RFC %s のHTMLを生成" % rfc)
-                make_html(rfc)
-                time.sleep(1)
-    elif args.summarize:
+    elif args.summarize and rfcs:
         # RFCの要約作成
         from src.summarize_rfc import summarize_rfc
         for rfc in rfcs:
@@ -108,13 +96,6 @@ if __name__ == '__main__':
                 # RFCのHTMLを作成
                 print("[*] RFC %s のHTMLを生成" % rfc)
                 make_html(rfc)
-    elif args.fetch and args.begin and args.end:
-        # 範囲指定でRFCの取得
-        print("[*] RFC %d - %d のRFCを取得" % (args.begin, args.end))
-        numbers = list(diff_remote_and_local_index())
-        numbers = [x for x in numbers if args.begin <= x <= args.end]
-        for rfc_number in numbers:
-            fetch_rfc(rfc_number)
     elif args.fetch and rfcs:
         # 指定したRFCの取得
         for rfc in rfcs:
@@ -125,17 +106,17 @@ if __name__ == '__main__':
         for rfc in rfcs:
             print("[*] RFC %s を翻訳" % rfc)
             trans_rfc(rfc)
-    elif args.make and args.begin and args.end:
-        # 範囲指定でRFCのHTML(rfcXXXX.html)を作成
-        print("[*] RFC %d - %d のHTMLを生成" % (args.begin, args.end))
-        for rfc_number in range(args.begin, args.end):
-            make_html(rfc_number)
     elif args.make and rfcs:
         # 指定したRFCのHTML(rfcXXXX.html)を作成
+        if len(rfcs) == 1:
+            print("[*] RFC %d のHTMLを生成" % (rfcs[0]))
+        elif len(rfcs) > 1:
+            print("[*] RFC %d - %d のHTMLを生成" % (rfcs[0], rfcs[-1]))
         for rfc in rfcs:
             make_html(rfc)
     elif args.make_json and rfcs:
         # 指定したRFCのJSONを翻訳修正したHTMLから逆作成
+        from src.make_json_from_html import make_json_from_html
         for rfc in rfcs:
             make_json_from_html(rfc)
     elif rfcs:
