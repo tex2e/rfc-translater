d8450c58f2099055c4a0a07f9a3e82230464ecb4
Mako <mnfeconicu41@gmail.com>
2024-02-18 10:59:36 +0900

    refactoring src

--- src/fetch_wg.py
+++ src/fetch_wg.py
@@ -5,37 +5,30 @@
 import os
 import re
 import json
-import requests
+# from pprint import pprint
 from lxml import etree
-from pprint import pprint
-
-# WorkingGroupのRFCの一覧の保存先
-OUTPUT_PATH = os.path.join("html", "group-rfcs.json")
-
-def fetch_url(url: str):
-    headers = {'User-agent': '', 'referer': url}
-    page = requests.get(url, headers, timeout=(36.2, 180))
-    return page
+from rfc_utils import RfcUtils
+from rfc_const import RfcXmlElem, RfcJsonElem, RfcFolder
 
 def write_rfc_wg_list():
-    page = fetch_url("https://www.rfc-editor.org/rfc-index.xml")
+    # WorkingGroupのRFCの一覧の保存先
+    OUTPUT_PATH = os.path.join(RfcFolder.OUTPUT_HTML_DIR, "group-rfcs.json")
 
-    # XMLの名前空間名を除去
-    page_content = re.sub(rb' xmlns(:xsi)?="[^"]+"', rb'', page.content, count=1)
-
+    page = RfcUtils.fetch_url("https://www.rfc-editor.org/rfc-index.xml")
+    page_content = RfcUtils.remove_namespace_from_xml(page.content)
     tree = etree.XML(page_content)
 
     obj = {}
 
-    for item in tree.xpath('/rfc-index/rfc-entry'):
+    for item in tree.xpath(f'/{RfcXmlElem.RFC_INDEX}/{RfcXmlElem.RFC_ENTRY}'):
         subtree = etree.XML(etree.tostring(item))
 
-        rfc_ids = subtree.xpath('/rfc-entry/doc-id/text()')
+        rfc_ids = subtree.xpath(f'/{RfcXmlElem.RFC_ENTRY}/{RfcXmlElem.DOC_ID}/text()')
         if len(rfc_ids) == 0:
             continue
         rfc_id = rfc_ids[0]
 
-        rfc_wgs = subtree.xpath('/rfc-entry/wg_acronym/text()')
+        rfc_wgs = subtree.xpath(f'/{RfcXmlElem.RFC_ENTRY}/{RfcXmlElem.WG_ACRONYM}/text()')
         if len(rfc_wgs) == 0:
             continue
         rfc_wg = rfc_wgs[0]
@@ -46,9 +39,64 @@ def write_rfc_wg_list():
             rfc_number = m.group(1)
             obj[rfc_number] = "wg/%s" % rfc_wg
 
-    with open(OUTPUT_PATH, 'w', encoding="utf-8", newline="\n") as f:
-        json.dump(obj, f, ensure_ascii=False, indent=2)
+    RfcUtils.write_json_file(OUTPUT_PATH, obj)
+
+
+def write_rfc_obsoletes():
+    # RFCステータスの一覧の保存先
+    OUTPUT_PATH = os.path.join(RfcFolder.OUTPUT_HTML_DIR, "obsoletes.json")
+
+    page = RfcUtils.fetch_url("https://www.rfc-editor.org/rfc-index.xml")
+    page_content = RfcUtils.remove_namespace_from_xml(page.content)
+    tree = etree.XML(page_content)
+
+    obj = {}
+
+    for item in tree.xpath(f'/{RfcXmlElem.RFC_INDEX}/{RfcXmlElem.RFC_ENTRY}'):
+        subtree = etree.XML(etree.tostring(item))
+
+        rfc_ids = subtree.xpath(f'/{RfcXmlElem.RFC_ENTRY}/{RfcXmlElem.DOC_ID}/text()')
+        if len(rfc_ids) == 0:
+            continue
+        rfc_id = rfc_ids[0]
+        if m := re.match(r'^RFC([0-9]+)$', rfc_id):
+            rfc_number = m.group(1)
+
+        obj[rfc_number] = {}
+
+        # obsoletes
+        rfc_obsoletes = subtree.xpath(f'/{RfcXmlElem.RFC_ENTRY}/{RfcXmlElem.OBSOLETES}/{RfcXmlElem.DOC_ID}/text()')
+        if len(rfc_obsoletes) > 0:
+            rfc_obsoletes = [RfcUtils.replace_rfcXXXX_to_XXXX(x) for x in rfc_obsoletes]
+            obj[rfc_number][RfcJsonElem.OBSOLETES] = rfc_obsoletes
+
+        # obsoleted-by
+        rfc_obsoleted_by = subtree.xpath(f'/{RfcXmlElem.RFC_ENTRY}/{RfcXmlElem.OBSOLETED_BY}/{RfcXmlElem.DOC_ID}/text()')
+        if len(rfc_obsoleted_by) > 0:
+            rfc_obsoleted_by = [RfcUtils.replace_rfcXXXX_to_XXXX(x) for x in rfc_obsoleted_by]
+            obj[rfc_number][RfcJsonElem.OBSOLETED_BY] = rfc_obsoleted_by
+
+        # updates
+        rfc_updates = subtree.xpath(f'/{RfcXmlElem.RFC_ENTRY}/{RfcXmlElem.UPDATES}/{RfcXmlElem.DOC_ID}/text()')
+        if len(rfc_updates) > 0:
+            rfc_updates = [RfcUtils.replace_rfcXXXX_to_XXXX(x) for x in rfc_updates]
+            obj[rfc_number][RfcJsonElem.UPDATES] = rfc_updates
+
+        # updated-by
+        rfc_updated_by = subtree.xpath(f'/{RfcXmlElem.RFC_ENTRY}/{RfcXmlElem.UPDATED_BY}/{RfcXmlElem.DOC_ID}/text()')
+        if len(rfc_updated_by) > 0:
+            rfc_updated_by = [RfcUtils.replace_rfcXXXX_to_XXXX(x) for x in rfc_updated_by]
+            obj[rfc_number][RfcJsonElem.UPDATED_BY] = rfc_updated_by
+
+        # current-status
+        rfc_current_status = subtree.xpath(f'/{RfcXmlElem.RFC_ENTRY}/{RfcXmlElem.CURRENT_STATUS}/text()')
+        if len(rfc_current_status) > 0:
+            rfc_current_status = RfcUtils.replace_upper_to_camel(rfc_current_status[0])
+            obj[rfc_number][RfcJsonElem.CURRENT_STATUS] = rfc_current_status
+
+    RfcUtils.write_json_file(OUTPUT_PATH, obj)
 
 
 if __name__ == '__main__':
-    write_rfc_wg_list()
+    # write_rfc_wg_list()
+    write_rfc_obsoletes()
--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@
 # ------------------------------------------------------------------------------
 
 import sys
-from src.fetch_rfc import fetch_rfc, RFCNotFound
-from src.trans_rfc import trans_rfc
-from src.make_html import make_html
-from src.make_index import make_index, make_index_draft
-from src.fetch_index import diff_remote_and_local_index
+from fetch_rfc import fetch_rfc, RFCNotFound
+from trans_rfc import trans_rfc
+from make_html import make_html
+from make_index import make_index, make_index_draft
+from fetch_index import diff_remote_and_local_index
 
 def main(rfc_number: int | str) -> None:
     print('[*] RFC %s:' % rfc_number)
@@ -40,25 +40,24 @@ def continuous_main(begin=None, end=None, only_first=False):
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--rfc',         type=str,            help='RFC number            (ex. --rfc 8446)')
-    parser.add_argument('--fetch',       action='store_true', help='Only fetch RFC        (ex. --rfc 8446 --fetch)')
-    parser.add_argument('--trans',       action='store_true', help='Only translate        (ex. --rfc 8446 --trans)')
-    parser.add_argument('--make',        action='store_true', help='Only make HTML        (ex. --rfc 8446 --fetch)')
-    parser.add_argument('--make-json',   action='store_true', help='Make JSON from HTML   (ex. --make-json --rfc 8446)')
-    parser.add_argument('--begin',       type=int,            help='Set begin rfc number  (ex. --begin 8000)')
-    parser.add_argument('--end',         type=int,            help='Set end rfc number    (ex. --begin 8000 --end 9000)')
-    parser.add_argument('--make-index',  action='store_true', help='Make html/index.html  (ex. --make-index)')
-    parser.add_argument('--force', '-f', action='store_true', help='Ignore cache          (ex. --rfc 8446 --fetch --force)')
-    parser.add_argument('--only-first',  action='store_true', help='Take only first RFC   (ex. --begin 8000 --only-first)')
-    parser.add_argument('--draft',       type=str,            help='Take RFC draft        (ex. --draft draft-ietf-tls-esni-14)')
-    parser.add_argument('--fetch-group', action='store_true', help='Write WG list         (ex. --fetch-group)')
-    parser.add_argument('--make-index-draft',
-                                         action='store_true', help='make draft/index.html (ex. --make-index-draft)')
-    parser.add_argument('--transtest',   action='store_true')
-    parser.add_argument('--summarize',   action='store_true', help='Summarize RFC by ChatGPT (ex. --summarize --rfc 8446)')
-    parser.add_argument('--chatgpt',     type=str,            help='ChatGPT model version    (ex. --chatgpt gpt-3.5-turbo)')
-    args = parser.parse_args()
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--rfc',              type=str,            help='RFC number            (ex. --rfc 8446)')
+    ap.add_argument('--fetch',            action='store_true', help='Only fetch RFC        (ex. --rfc 8446 --fetch)')
+    ap.add_argument('--trans',            action='store_true', help='Only translate        (ex. --rfc 8446 --trans)')
+    ap.add_argument('--make',             action='store_true', help='Only make HTML        (ex. --rfc 8446 --fetch)')
+    ap.add_argument('--make-json',        action='store_true', help='Make JSON from HTML   (ex. --make-json --rfc 8446)')
+    ap.add_argument('--begin',            type=int,            help='Set begin rfc number  (ex. --begin 8000)')
+    ap.add_argument('--end',              type=int,            help='Set end rfc number    (ex. --begin 8000 --end 9000)')
+    ap.add_argument('--make-index',       action='store_true', help='Make html/index.html  (ex. --make-index)')
+    ap.add_argument('--force', '-f',      action='store_true', help='Ignore cache          (ex. --rfc 8446 --fetch --force)')
+    ap.add_argument('--only-first',       action='store_true', help='Take only first RFC   (ex. --begin 8000 --only-first)')
+    ap.add_argument('--draft',            type=str,            help='Take RFC draft        (ex. --draft draft-ietf-tls-esni-14)')
+    ap.add_argument('--fetch-status',     action='store_true', help='Make group-rfcs.json and obsoletes.json')
+    ap.add_argument('--make-index-draft', action='store_true', help='Make draft/index.html (ex. --make-index-draft)')
+    ap.add_argument('--transtest',        action='store_true')
+    ap.add_argument('--summarize',        action='store_true', help='Summarize RFC by ChatGPT (ex. --summarize --rfc 8446)')
+    ap.add_argument('--chatgpt',          type=str,            help='ChatGPT model version    (ex. --chatgpt gpt-3.5-turbo)')
+    args = ap.parse_args()
 
     # RFCの指定（複数の場合はカンマ区切り）
     rfcs = None
@@ -70,20 +69,18 @@ if __name__ == '__main__':
         rfcs = [args.draft]
 
     if args.make_index:
-        # トップページ(index.html)の作成
-        print("[*] index.htmlの作成")
+        print("[*] トップページ(index.html)の作成")
         make_index()
     elif args.make_index_draft:
-        # トップページ(draft/index.html)の作成
         print("[*] draft/index.htmlの作成")
         make_index_draft()
-    elif args.fetch_group:
-        # WorkingGroupの一覧作成
-        print("[*] WorkingGroupの一覧作成")
-        from src.fetch_wg import write_rfc_wg_list
+    elif args.fetch_status:
+        print("[*] RFCの更新状況とWorkingGroupの一覧作成")
+        from src.fetch_wg import write_rfc_wg_list, write_rfc_obsoletes
         write_rfc_wg_list()
+        write_rfc_obsoletes()
     elif args.transtest:
-        # 翻訳のテスト
+        print("[*] 翻訳テスト開始...")
         from src.trans_rfc import trans_test
         res = trans_test()
         print('Translate test result:', res)
@@ -97,17 +94,17 @@ if __name__ == '__main__':
                 print("[*] RFC %s のHTMLを生成" % rfc)
                 make_html(rfc)
     elif args.fetch and rfcs:
-        # 指定したRFCの取得
+        # 指定したRFCの取得 (rfcXXXX.json)
         for rfc in rfcs:
             print("[*] RFC %s を取得" % rfc)
             fetch_rfc(rfc, args.force)
     elif args.trans and rfcs:
-        # RFCの翻訳
+        # RFCの翻訳 (rfcXXXX-trans.json)
         for rfc in rfcs:
             print("[*] RFC %s を翻訳" % rfc)
             trans_rfc(rfc)
     elif args.make and rfcs:
-        # 指定したRFCのHTML(rfcXXXX.html)を作成
+        # RFCのHTMLを作成 (rfcXXXX.html)
         if len(rfcs) == 1:
             print("[*] RFC %d のHTMLを生成" % (rfcs[0]))
         elif len(rfcs) > 1:
@@ -127,6 +124,6 @@ if __name__ == '__main__':
         # 未翻訳のRFCを順番に取得・翻訳・作成
         continuous_main(begin=args.begin, end=args.end, only_first=args.only_first)
     else:
-        parser.print_help()
+        ap.print_help()
 
 print("[+] 正常終了 %s" % sys.argv[0])
